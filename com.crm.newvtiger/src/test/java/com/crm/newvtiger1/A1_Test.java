9f2b8cb226271ee9e5f6762a812e7033333559ed
hp <hp@DESKTOP-SNCN38L>
2022-03-28 14:39:41 +0530

    hhh

---
+++ com.crm.newvtiger/src/test/java/com/crm/newvtiger1/A1_Test.java
@@ -0,0 +1,23 @@
+package com.crm.newvtiger1;
+
+import org.testng.annotations.Test;
+
+public class A1_Test {
+	
+	@Test
+	public void a1 () {
+		System.out.println("a1 running");
+	}
+	@Test
+	public void a2() {
+		System.out.println("a2 running");
+	}
+	@Test
+	public void a3 () {
+		System.out.println("a3 running");
+	}
+	@Test
+	public void a4 () {
+		System.out.println("a4 running");
+	}
+}
---
+++ com.crm.newvtiger/src/test/java/com/crm/newvtiger1/B1_Test.java
@@ -0,0 +1,23 @@
+package com.crm.newvtiger1;
+
+import org.testng.annotations.Test;
+
+public class B1_Test {
+	
+	@Test
+	public void b1 () {
+		System.out.println("b1 running");
+	}
+	@Test
+	public void b2() {
+		System.out.println("b2 running");
+	}
+	@Test
+	public void b3 () {
+		System.out.println("b3 running");
+	}
+	@Test
+	public void b4 () {
+		System.out.println("b4 running");
+	}
+}
